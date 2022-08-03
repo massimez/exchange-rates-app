58de7e39480439429baedb456ab2e4263bb8e380
Massi <massi.mez@ya.ru>
2022-08-04 04:32:50 +0700

    feat: add base layout

---
+++ src/layouts/defaultLayout.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+<template>
+  <div class="wrapper">
+    <header></header>
+    <main class="main">
+      <slot></slot>
+    </main>
+    <!-- <footer></footer> -->
+  </div>
+</template>
+<style lang="scss" scoped>
+.wrapper {
+  min-height: 100vh;
+  padding-top: 20px;
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient-background 20s ease infinite;
+}
+.main {
+  display: flex;
+  width: 700px;
+  margin: auto;
+  height: 700px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.45);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.1px);
+  -webkit-backdrop-filter: blur(5.1px);
+  border: 1px solid rgba(255, 255, 255, 0.68);
+}
+
+@include _lg {
+  .main {
+    width: 600px;
+  }
+}
+@include _xs {
+  .main {
+    width: 100%;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import CurrenciesList from "../components/CurrenciesList.vue";
+import { ref, onMounted } from "vue";
+import { useStore } from "vuex";
+import BaseTabs from "../components/BaseTabs.vue";
+import ConversionRate from "../components/ConversionRate.vue";
+
+const store = useStore();
+onMounted(async () => {
+  await store.dispatch("fetCurrenciesRateChange");
+});
+const tabs = ref(["Exchange Rate", "Conversion"]);
+const activeTab = ref("Exchange Rate");
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container-content">
+    <BaseTabs
+      :tabs="tabs"
+      :active-tab="activeTab"
+      @change_tab="activeTab = $event"
+    ></BaseTabs>
+    <Transition mode="out-in" name="fade-in">
+      <CurrenciesList v-if="activeTab === 'Exchange Rate'" />
+      <ConversionRate v-else-if="activeTab === 'Conversion'" />
+    </Transition>
+  </div>
 </template>
+<style lang="scss" scoped>
+.container-content {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+</style>
